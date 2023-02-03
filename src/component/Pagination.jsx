e5eef112c6d6a5d24d0cb92c17a847be7767b01b
badnaim <batnyaam.tumurbat@gmail.com>
2023-02-03 08:19:48 +0800

    b

--- src/component/Pagination.jsx
+++ src/component/Pagination.jsx
@@ -2,13 +2,25 @@ import PageBtn from "./subcomp/PageBtn";
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 
-// const PageNumbers = 3456;
+const PageNumbers = 3456;
 export default function Pagination() {
-  const [currentPage, setCurrentPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="pag">
       <div className="buttons">
-        <PageBtn id="words" btnName={"Өмнөх"} btnClass={"inactive"} />
+
+        {/* <link to={`/page/${currentPage - 1}`}
+            onClick={() => setCurrentPage(currentPage - 1)}> */}
+          <PageBtn id="words" btnName={"Өмнөх"} btnClass={"inactive"} />
+        {/* </link> */}
+
+        {/* <Link
+            to={`/page/${currentPage - 2}`}
+            onClick={() => setCurrentPage(currentPage - 2)}
+          >
+            <PageBtn btnName={currentPage - 2} btnClass={"inactive"} />
+          </Link> */}
+        
         <div className="numbers">
           <Link
             to={`/page/${currentPage - 1}`}
@@ -28,10 +40,31 @@ export default function Pagination() {
             <PageBtn btnName={currentPage + 1} btnClass={"inactive"} />
           </Link>
 
-          {/* <p>...</p>
-          <PageBtn btnName={PageNumbers} /> */}
+          <Link
+            to={`/page/${currentPage + 2}`}
+            onClick={() => setCurrentPage(currentPage + 2)}
+          >
+            <PageBtn btnName={currentPage + 2} btnClass={"inactive"} />
+          </Link>
+          <span>
+          <p>...</p>
+          </span>
+          
+
+          <Link
+            to={`/page/${currentPage + 3455}`}
+            onClick={() => setCurrentPage(currentPage + 3455)}
+          >
+            <PageBtn btnName={PageNumbers} btnClass={"inactive"}/>
+          </Link>
+          
         </div>
+
+        {/* <link to={`/page/${currentPage + 1}`}
+            onClick={() => setCurrentPage(currentPage + 1)}> */}
         <PageBtn id="words" btnName={"Дараах"} btnClass={"inactive"} />
+        {/* </link> */}
+        
       </div>
     </div>
   );
